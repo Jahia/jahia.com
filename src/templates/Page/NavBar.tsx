cb8ce386af2fca37ec9cc64ef63fac8e5cecc499
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-04-16 12:37:56 +0200

    feat: make menu CTAs editable

--- src/templates/Page/NavBar.tsx
+++ src/templates/Page/NavBar.tsx
@@ -6,18 +6,40 @@ import {
 } from "@jahia/javascript-modules-library";
 import type { JCRNodeWrapper } from "org.jahia.services.content";
 import NavBarClient from "./NavBar.client.jsx";
+import type { JCRSiteNode } from "org.jahia.services.content.decorator";
 
 export default function NavBar({
+  site,
   root,
   current,
 }: {
+  site: JCRSiteNode;
   root: JCRNodeWrapper;
   current: JCRNodeWrapper;
 }) {
+  const primaryCTALink =
+    site.hasProperty("primaryCTALink") && site.getProperty("primaryCTALink").getValue().getNode();
+  const secondaryCTALink =
+    site.hasProperty("secondaryCTALink") &&
+    site.getProperty("secondaryCTALink").getValue().getNode();
+
   return (
     <HydrateInBrowser
       child={NavBarClient}
       props={{
+        // Menu CTAs
+        primaryCTA: primaryCTALink && {
+          href: buildNodeUrl(primaryCTALink),
+          label:
+            site.getPropertyAsString("primaryCTALabel") ||
+            primaryCTALink.getPropertyAsString("jcr:title"),
+        },
+        secondaryCTA: secondaryCTALink && {
+          href: buildNodeUrl(secondaryCTALink),
+          label:
+            site.getPropertyAsString("secondaryCTALabel") ||
+            secondaryCTALink.getPropertyAsString("jcr:title"),
+        },
         // This can quickly get out of hand, if there are too many pages in the menu we need
         // to rethink the implementation
         pages: getChildNodes(root, -1, 0, (node) => node.isNodeType("jnt:page")).map((node) => ({
