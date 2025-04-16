435ff2989742ff119348d9126d773dca1d8af4f3
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-04-16 10:41:31 +0200

    feat: new hero stack view

--- src/components/Hero/types.ts
+++ src/components/Hero/types.ts
@@ -1,12 +1,8 @@
 import type { JCRNodeWrapper } from "org.jahia.services.content";
+import type { CTAProps } from "./CTA.jsx";
 
 export type Props = {
   "jcr:title"?: string;
   "subtitle"?: string;
   "image": JCRNodeWrapper;
-  "ctaLabel"?: string;
-} & ( // Reflect the three possible values of j:linkType
-  | { "j:linkType": "none" }
-  | { "j:linkType": "external"; "j:url": string; "j:linkTitle": string }
-  | { "j:linkType": "internal"; "j:linknode": JCRNodeWrapper }
-);
+} & ({ "j:linkType": "none" } | CTAProps);
