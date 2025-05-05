11914a7cf151c6d96228be3e75a478399d50cfd4
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-05-05 18:07:32 +0200

    theme generic container

--- src/views/GenericContainer/default.server.tsx
+++ src/views/GenericContainer/default.server.tsx
@@ -5,17 +5,15 @@ import {
   Render,
 } from "@jahia/javascript-modules-library";
 import classes from "./component.module.css";
+import type { ThemeProps } from "../../theme.js";
 
 jahiaComponent(
   {
     componentType: "view",
     nodeType: "jahiacom:genericContainer",
   },
-  (
-    { theme, columns }: { theme: "day" | "cloudy" | "night"; columns: "100" | "50-50" },
-    { currentNode },
-  ) => (
-    <section className={classes.container} data-theme={theme}>
+  ({ theme, background, columns }: { columns: "100" | "50-50" } & ThemeProps, { currentNode }) => (
+    <section className={classes.container} data-theme={theme} data-bg={background}>
       <div style={{ gridTemplateColumns: { "50-50": "1fr 1fr" }[columns as never] ?? "1fr" }}>
         {getChildNodes(currentNode, -1, 0, (node) => node.isNodeType("jnt:content")).map((node) => (
           <div key={node.getIdentifier()}>
