ab90c1f82c46242cc4e500ad270d2b321d016dee
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-05-22 10:47:51 +0200

    perf: add <link preload> for cover hero

--- src/views/Hero/cover.server.tsx
+++ src/views/Hero/cover.server.tsx
@@ -11,21 +11,24 @@ jahiaComponent(
     name: "cover",
   },
   ({ theme, "jcr:title": title, subtitle, image, ...cta }: Props) => (
-    <header
-      // Despite being mandatory, the image can be missing in some cases (e.g. new translation)
-      style={{ backgroundImage: image && `url(${buildNodeUrl(image)})`, paddingBlock: "4rem" }}
-      className={classes.hero}
-      data-theme={theme}
-    >
-      <div className={clsx(classes.header, "_stack-4")}>
-        <h1>{title || "Title not defined"}</h1>
-        {subtitle && <p>{subtitle}</p>}
-        {cta.ctaType !== "none" && (
-          <p>
-            <LinkTypeCTA cta={cta} />
-          </p>
-        )}
-      </div>
-    </header>
+    <>
+      {image && <link rel="preload" as="image" href={buildNodeUrl(image)} />}
+      <header
+        // Despite being mandatory, the image can be missing in some cases (e.g. new translation)
+        style={{ backgroundImage: image && `url(${buildNodeUrl(image)})`, paddingBlock: "4rem" }}
+        className={classes.hero}
+        data-theme={theme}
+      >
+        <div className={clsx(classes.header, "_stack-4")}>
+          <h1>{title || "Title not defined"}</h1>
+          {subtitle && <p>{subtitle}</p>}
+          {cta.ctaType !== "none" && (
+            <p>
+              <LinkTypeCTA cta={cta} />
+            </p>
+          )}
+        </div>
+      </header>
+    </>
   ),
 );
