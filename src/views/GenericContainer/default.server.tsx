08e71f087e7b021b3916872ebcb5d3a5ce29a814
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-05-23 12:36:03 +0200

    feat: add background to column items option

--- src/views/GenericContainer/default.server.tsx
+++ src/views/GenericContainer/default.server.tsx
@@ -6,6 +6,7 @@ import {
 } from "@jahia/javascript-modules-library";
 import { Container, type ContainerProps } from "../../theme/index.js";
 import classes from "./component.module.css";
+import clsx from "clsx";
 
 type Columns = "100" | "50-50" | "67-33" | "33-67" | "33-33-33" | "25-25-25-25";
 type Width = "100" | "75" | "50";
@@ -16,13 +17,18 @@ jahiaComponent(
     nodeType: "jahiacom:genericContainer",
   },
   (
-    { columns, width, ...props }: { columns: Columns; width: Width } & ContainerProps,
+    {
+      columns,
+      width,
+      itemize,
+      ...props
+    }: { columns: Columns; width: Width; itemize?: boolean } & ContainerProps,
     { currentNode },
   ) => (
     <Container {...props}>
       <div className={classes.grid} data-columns={columns} data-width={width}>
         {getChildNodes(currentNode, -1, 0, (node) => node.isNodeType("jnt:content")).map((node) => (
-          <div key={node.getIdentifier()}>
+          <div className={clsx(itemize && classes.item)} key={node.getIdentifier()}>
             <Render node={node} />
           </div>
         ))}
