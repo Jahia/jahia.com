2a3d30242ad429dd2d56dd9fe879a63843c0a1ba
AlexDechosal <alexandre.dechosal@free.fr>
2025-11-05 17:05:02 +0100

    feat: ajout du nouveau composant Testimony
    - Ajout du composant Testimony avec sa définition (definition.cnd)
    - Ajout des styles du composant Testimony (styles.module.css)
    - Ajout des vues par défaut et cover (default.server.tsx, cover.server.tsx)

---
+++ src/views/Testimony/default.server.tsx
@@ -0,0 +1,40 @@
+import { jahiaComponent } from "@jahia/javascript-modules-library";
+import type { JCRNodeWrapper } from "org.jahia.services.content";
+import { MixinCTA, type CTAProps } from "../../mixins/CTA/server.jsx";
+import classes from "./styles.module.css";
+import { Image } from "../../components/Image.jsx";
+
+type Props = {
+  author?: string;
+  quote: string;
+  image?: JCRNodeWrapper;
+  theme?: string;
+  background?: string;
+} & CTAProps;
+
+jahiaComponent(
+  {
+    componentType: "view",
+    nodeType: "jahiacom:testimony",
+  },
+  ({ author, quote, image, theme, background, ...cta }: Props) => (
+    <section className={classes.container} data-theme={theme} data-bg={background}>
+      <div className={classes.testimony}>
+        <div className={classes.imageTestimony}>
+          {image && <Image image={image} className={classes.image} />}
+        </div>
+        <div className={classes.text}>
+          <div className="_richtext">
+            <blockquote dangerouslySetInnerHTML={{ __html: quote }}></blockquote>
+          </div>
+          {author && <p className={classes.author}>— {author}</p>}
+          {cta.ctaType !== "none" && (
+            <p style={{ marginTop: "1rem" }}>
+              <MixinCTA cta={cta} />
+            </p>
+          )}
+        </div>
+      </div>
+    </section>
+  ),
+);
