75ccde6b4e6c5f3e875adbc08f9d7dfb33cb3700
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-12-18 17:40:24 +0100

    fix: don't display full blog entry contents in default view

--- src/contents/Blog/Entry/default.server.tsx
+++ src/contents/Blog/Entry/default.server.tsx
@@ -6,20 +6,13 @@ jahiaComponent(
     componentType: "view",
     nodeType: "jahiacom:blogEntry",
   },
-  ({ "jcr:title": title, author, blogType, date, image, summary, text }: Props) => (
+  ({ "jcr:title": title, author, date, image, summary }: Props) => (
     <ul>
       <li>title: {title || "no title"}</li>
       <li>author: {author ? author.getPath() : "no author"}</li>
       <li>date: {date || "no date"}</li>
       <li>image: {image ? buildNodeUrl(image) : "no image"}</li>
       <li>summary: {summary || "no summary"}</li>
-      <li>text: {text || "no text"}</li>
-      <li>
-        blogType:{" "}
-        {blogType && blogType.length > 0
-          ? blogType.map((bt) => (bt ? bt.getPath() : "null")).join(", ")
-          : "no blogType"}
-      </li>
     </ul>
   ),
 );
