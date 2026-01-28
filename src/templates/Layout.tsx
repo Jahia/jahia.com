e18bfe3d5b158043e1d641b5e119c5b9543477b7
Gautier Ben Aïm <benaim.gautier@gmail.com>
2026-01-28 17:56:44 +0100

    feat: gtm container

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -29,6 +29,7 @@ interface Props {
 export const Layout = ({ props, children }: { props: Props; children: ReactNode }) => {
   const { currentResource, renderContext, mainNode } = useServerContext();
   const lang = currentResource.getLocale().getLanguage();
+  const site = renderContext.getSite();
 
   const {
     "jcr:title": title,
@@ -43,6 +44,13 @@ export const Layout = ({ props, children }: { props: Props; children: ReactNode 
   return (
     <html lang={lang}>
       <head>
+        {site.hasProperty("gtmId") && (
+          <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',${JSON.stringify(site.getPropertyAsString("gtmId"))});`}</script>
+        )}
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{htmlTitle || title}</title>
