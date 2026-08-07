e93d5dc4e9c3d088d3e392b51ae872c98dae8927
Dominique Gigon <dgigon@jahia.com>
2026-08-07 09:39:09 +0200

    fix: flush the blog feed fragment when a new entry is published (#123)

--- src/views/ContentFolderReference/blog.server.tsx
+++ src/views/ContentFolderReference/blog.server.tsx
@@ -1,4 +1,4 @@
-import { jahiaComponent, Render, useJCRQuery } from "@jahia/javascript-modules-library";
+import { jahiaComponent, Render, server, useJCRQuery } from "@jahia/javascript-modules-library";
 import clsx from "clsx";
 import type { JCRNodeWrapper } from "org.jahia.services.content";
 import classes from "./styles.module.css";
@@ -13,16 +13,28 @@ jahiaComponent(
     nodeType: "jnt:contentFolderReference",
     name: "blog",
   },
-  ({ "j:node": folder }: Props, { renderContext }) => (
-    <div className={clsx(renderContext.isEditMode() || classes.grid)}>
-      {folder &&
-        useJCRQuery({
-          query: `
+  ({ "j:node": folder }: Props, { renderContext }) => {
+    // Jahia only registers cache dependencies on the entries rendered below, so an entry that did
+    // not exist yet when this fragment was cached cannot flush it: publishing a new blog post left
+    // the list stale until eviction. Depending on the folder subtree flushes it on any addition.
+    if (folder) {
+      server.render.addCacheDependency(
+        { flushOnPathMatchingRegexp: `${folder.getPath()}/.*` },
+        renderContext,
+      );
+    }
+
+    return (
+      <div className={clsx(renderContext.isEditMode() || classes.grid)}>
+        {folder &&
+          useJCRQuery({
+            query: `
             SELECT * FROM [jahiacom:blogEntry]
             WHERE ISDESCENDANTNODE(${JSON.stringify(folder.getPath())})
             ORDER BY [date] DESC
           `,
-        }).map((entry) => <Render key={entry.getIdentifier()} node={entry} />)}
-    </div>
-  ),
+          }).map((entry) => <Render key={entry.getIdentifier()} node={entry} />)}
+      </div>
+    );
+  },
 );
